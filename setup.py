396e9cdd8ed7af2e46841e0657b4cb541c0da9c7
Redstar <aliakbarzarei41@gmail.com>
2025-07-09 14:08:25 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='inkaterm',
-    version='1.0.2',
+    version='1.0.3',
     description='Convert PNG images to ASCII colored art',
     author='redstar1228',
     author_email='aliakbarzarei41@gmail.com',
