c22021312c1cb416667f7a78fd948c2cf46939e0
Redstar <aliakbarzarei41@gmail.com>
2025-07-09 14:07:50 +0330

    More accuracy

--- inkaterm/process/__init__.py
+++ inkaterm/process/__init__.py
@@ -15,19 +15,19 @@ def main(file, char, same):
             n = "black"
         elif r > g and b > g and b > 100 and r > 100 and (r - g > 60 or b - g > 60):
             n = "magenta"
+        elif r > 44 and g > 44 and b > 40 and r < 180 and g < 180 and b < 180 and (r - b < 26 or r - b > -26) and (b - g < 26 or b - g > -26) and (r - g < 26 or r - g > -26):
+            n = "dark_grey"
         elif r < g and b > 40 and r - b < -30:
             if b < 100:
                 n = "blue"
             else:
                 n = "cyan"
-        elif g > b and g > r and g > 40 and g < 256:
+        elif g > b and g > r and g > 40 and g - r > 60 and r + b < g:
             n = "green"
-        elif r > b and r > g and r > 60 and r < 256 and g < 50:
+        elif r > b and r > g and r > 60 and g < 50:
             n = "red"
-        elif (r - g < 80 or g - r > -80) and b < 100 and g > 60:
+        elif (r - g < 80 or g - r > -80) and b < 100 and g > 100:
             n = "yellow"
-        elif r > 44 and g > 44 and b > 40 and r < 201 and g < 201 and b < 201:
-            n = "dark_grey"
         else:
             n = "white"
         x.append(colored(char, n, on_color = f"on_{n}" if same else None))
