cef639fedf66ac97bcd61db20dc760f04de433c3
Redstar <aliakbarzarei41@gmail.com>
2025-08-11 13:26:48 +0330

    pro added to __init__.py

--- inkaterm/__init__.py
+++ inkaterm/__init__.py
@@ -1,4 +1,4 @@
 from inkaterm.procces import main
 
-def ink(file, char = "# ", same = True):
-    return main(file, char, same)
+def ink(file, char = "# ", same = True, pro = {"key": None, "report": False}):
+    return main(file, char, same, pro)
