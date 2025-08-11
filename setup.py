03cba1d87e39053c0b4119b57cf00cb14d11d156
Redstar <aliakbarzarei41@gmail.com>
2025-08-11 13:30:55 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='inkaterm',
-    version='1.0.3',
+    version='1.1.0',
     description='Convert PNG images to ASCII colored art',
     author='redstar1228',
     author_email='aliakbarzarei41@gmail.com',
     packages=find_packages(include=["inkaterm", "inkaterm.*"]),
+    include_package_data=True,
+    package_data={
+        "inkaterm": ["db/*.json"],
+    },
     install_requires=[
         'termcolor',
         'pillow',
